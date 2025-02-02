67c3aca6962537c21945d83d9d177a2901d46994
Andrew Karanja Njiyo <karanjaandrew2000@outlook.com>
2025-02-02 21:20:06 +0300

    set up the home nav

---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+      <div className="card">
+        <h2>EcoCollect </h2>
+        <p>Welcome to the EcoCollect home page! This application helps connect residents and waste collectors for efficient, eco-friendly pickups.</p>
+      </div>
+    );
+  }
+  
+  export default Home;
