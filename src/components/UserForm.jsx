59d7288d033c0c4bc53e046ae6d20d5da63bd375
Andrew Karanja Njiyo <karanjaandrew2000@outlook.com>
2025-02-02 22:15:03 +0300

    userform validation schema

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -5,9 +5,23 @@ import { useNavigate } from "react-router-dom";  // <-- NEW IMPORT
 import BASE_URL from "../config";
 
 function UserForm() {
-    const [serverError, setServerError] = useState("");
-    const [successMessage, setSuccessMessage] = useState("");
-  
+  const [serverError, setServerError] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+  // React Router hook for navigating
+  const navigate = useNavigate();
+
+  // Yup validation schema
+  const validationSchema = Yup.object({
+    username: Yup.string()
+      .required("Username is required")
+      .min(3, "Must be 3 chars or more"),
+    password: Yup.string()
+      .required("Password is required")
+      .min(6, "Must be 6 chars or more"),
+    role: Yup.string()
+      .oneOf(["resident", "collector"], "Invalid role")
+      .required("Role is required"),
+  });  
   }
   
   export default UserForm;
