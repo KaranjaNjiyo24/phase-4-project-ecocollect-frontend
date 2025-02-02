39e32e273b442150eafc41775966d0017b44c9f6
Andrew Karanja Njiyo <karanjaandrew2000@outlook.com>
2025-02-02 21:23:40 +0300

    Sets up router links on the webpage

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,29 @@ import AssignmentsPage from "./components/AssignmentsPage";
 import UserForm from "./components/UserForm";
 
 function App() {
+  return (
+    <Router>
+      {/* Navigation bar is styled by nav { ... } in EcoCollect.css */}
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/users">Users</Link>
+        <Link to="/pickup-requests">Pickup Requests</Link>
+        <Link to="/assignments">Assignments</Link>
+        <Link to="/new-user">Create User</Link>
+      </nav>
 
+      {/* Main container for the page content */}
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/users" element={<UsersPage />} />
+          <Route path="/pickup-requests" element={<PickupRequestsPage />} />
+          <Route path="/assignments" element={<AssignmentsPage />} />
+          <Route path="/new-user" element={<UserForm />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App
