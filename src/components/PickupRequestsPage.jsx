8002fc85bd5d9c2e5d26000a5072bc01a5bb869d
Andrew Karanja Njiyo <karanjaandrew2000@outlook.com>
2025-02-02 22:05:44 +0300

    Pickup requests

---
+++ src/components/PickupRequestsPage.jsx
@@ -0,0 +1,9 @@
+// src/components/PickupRequestsPage.js
+import React, { useState, useEffect } from "react";
+import BASE_URL from "../config";
+
+function PickupRequestsPage() {
+
+}
+
+export default PickupRequestsPage;
